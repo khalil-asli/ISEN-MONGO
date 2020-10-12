ed871b734b347ef5399f1aed3c63ea676afc940d
khalil-asli <56821845+khalil-asli@users.noreply.github.com>
2020-10-12 20:08:53 +0200

    Update Worker_Quest2.py

--- Worker_Quest2.py
+++ Worker_Quest2.py
@@ -7,38 +7,43 @@ import requests
 import json
 from pprint import pprint
 from pymongo import MongoClient
+import time
+import dateutil.parser
 
- 
+client = MongoClient("mongodb+srv://123456:khalil1234@cluster0.welnc.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client.vls 
-collection_vlilles = db.vlilles 
+db.updateddata.create_index([('station_id', 1),('date', -1)], unique=True)
 
- 
-
-def get_velib(url):
-    payload = {}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
+def get_vlille():
+    url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=-1&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
+    response = requests.request("GET", url)
     response_json = json.loads(response.text.encode('utf8'))
     return response_json.get("records", [])
 
- 
+def get_station_id(id_ext):
+    tps = db.vLille.find_one({ 'source.id_ext': id_ext }, { '_id': 1 })
+    return tps['_id']
 
+while True:
+    print('update')
+    vlilles = get_vlille()
+    updateddata = [
+        {
+            "bike_availbale": elem.get('fields', {}).get('nbvelosdispo'),
+            "stand_availbale": elem.get('fields', {}).get('nbplacesdispo'),
+            "date": dateutil.parser.parse(elem.get('fields', {}).get('datemiseajour')),
+            "station_id": get_station_id(elem.get('fields', {}).get('libelle'))
+        }
+        for elem in vlilles
+    ]
 
-vlilles = get_velib("https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=-1")
+    try:
+        db.updateddata.insert_many(updateddata, ordered=False)
+    except:
+        pass
 
- 
-
-
-vlilles_format = []
-for vlib in vlilles:
-    vlilles_format.append({
-        "name": vlib["fields"]["nom"],
-        "vlilles_dispo": vlib["fields"]["nbvelosdispo"],
-        "places_dispo" : vlib["fields"]["nbplacesdispo"],
-        "status": vlib["fields"]["etat"] == "EN SERVICE",
-        "record_timestamp": vlib["record_timestamp"] })
+    time.sleep(10)
 
  
 
